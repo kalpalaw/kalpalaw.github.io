676c9122cbfc753316a19bde8557ece2c0c9509d
Kalpa Law <mail@kalpalaw.in>
2024-08-13 00:58:43 -0400

    Re-enable disclaimer

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ import './components/MenuButton.js';
 const dialog = document.querySelector("dialog");
 const closeButton = document.querySelector("dialog button");
 
-// dialog.showModal();
+dialog.showModal();
 
 // "Close" button closes the dialog
 closeButton.addEventListener("click", () => {
